c77536e7102e9ff505d3006852624c01b445ff30
umpire-3 <umpire333@gmail.com>
2018-03-05 18:41:01 +0200

    bower.json fix.

--- app/app.js
+++ app/app.js
@@ -1,29 +1,38 @@
 'use strict';
 
 angular.module('dashboardApp', ['authorizationPage', 'stockPanelList', 'services'])
-    .controller('dashboardApp', ['$scope', 'authorizationService', function ($scope, auth) {
-        $scope.isSignedIn = false;
+    .controller('dashboardApp', [
+        '$scope', 'authorizationService',
+        function ($scope, auth) {
+            $scope.isSignedIn = false;
 
-        auth.init(function listen(isSignedIn) {
-            if (isSignedIn) {
-                $scope.promt = 'Sign Out';
-            }
-            else {
-                $scope.promt = 'Sign In';
-            }
+            auth.init(function listen(isSignedIn) {
+                if (isSignedIn) {
+                    $scope.promt = 'Sign Out';
+                }
+                else {
+                    $scope.promt = 'Sign In';
+                }
 
-            $scope.$apply(function (scope) {
-                scope.isSignedIn = isSignedIn;
+                $scope.$apply(function (scope) {
+                    scope.isSignedIn = isSignedIn;
+                });
             });
-        });
 
-        $scope.signIn = function () {
-            if (auth.isAuthorized) {
-                auth.getInstance().signOut();
-                auth.getInstance().disconnect();
-            }
-            else {
-                auth.getInstance().signIn();
-            }
+            $scope.signIn = function () {
+                if (auth.isAuthorized) {
+                    auth.getInstance().signOut();
+                    auth.getInstance().disconnect();
+                }
+                else {
+                    auth.getInstance().signIn();
+                }
+            };
+
+            this.$onInit = function () {
+                $('#singButton').css({
+                    display: 'block'
+                });
+            };
         }
-    }]);
+    ]);
