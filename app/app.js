da9d57b60991c14efd5ab89da0d3feab2b9bfa7c
Ilya <dadofasting@gmail.com>
2018-03-06 00:13:04 +0200

    Authorization page complited

--- app/app.js
+++ app/app.js
@@ -3,36 +3,30 @@
 angular.module('dashboardApp', ['authorizationPage', 'stockPanelList', 'services'])
     .controller('dashboardApp', [
         '$scope', 'authorizationService',
-        function ($scope, auth) {
+        function($scope, auth) {
             $scope.isSignedIn = false;
 
             auth.init(function listen(isSignedIn) {
-                if (isSignedIn) {
-                    $scope.promt = 'Sign Out';
-                }
-                else {
-                    $scope.promt = 'Sign In';
-                }
+                console.log('mainCtrl: ' + isSignedIn);
 
-                $scope.$apply(function (scope) {
+                $scope.$apply(function(scope) {
                     scope.isSignedIn = isSignedIn;
                 });
             });
 
-            $scope.signIn = function () {
+            $scope.signIn = function() {
                 if (auth.isAuthorized) {
                     auth.getInstance().signOut();
                     auth.getInstance().disconnect();
-                }
-                else {
+                } else {
                     auth.getInstance().signIn();
                 }
             };
 
-            this.$onInit = function () {
-                $('#singButton').css({
-                    display: 'block'
-                });
-            };
+            // this.$onInit = function() {
+            //     $('#signButton').css({
+            //         display: 'block'
+            //     });
+            // };
         }
     ]);
--- app/authorization-page/authorization-page.component.js
+++ app/authorization-page/authorization-page.component.js
@@ -3,7 +3,35 @@
 angular.module('authorizationPage')
     .component('authorizationPage', {
         templateUrl: 'authorization-page/authorization-page.template.html',
-        controller: [function () {
-            var ctrl = this;
-        }]
+        controller: ['$scope', function($scope) {
+            var $ctrl = this;
+
+            $ctrl.$onInit = function() {
+                console.dir($ctrl.onSignIn);
+                console.dir($ctrl);
+                $('.modal').modal();
+                $('.modal').modal({
+                    dismissible: false, // Modal can be dismissed by clicking outside of the modal
+                    opacity: .5, // Opacity of modal background
+                    inDuration: 300, // Transition in duration
+                    outDuration: 200, // Transition out duration
+                    startingTop: '35%', // Starting top style attribute
+                    endingTop: '20%', // Ending top style attribute
+
+                });
+                $('.modal').css('width', '25%');
+                $('#modal1').modal('open');
+            };
+
+            $ctrl.$onDestroy = function() {
+                $('#modal1').modal('close');
+            };
+
+            $ctrl.signIn = function() {
+                $ctrl.onSignIn();
+            };
+        }],
+        bindings: {
+            onSignIn: '&'
+        }
     });
