cd11af828a2e8342cba3753104ae4cfa167eef5a
Ilya <dadofasting@gmail.com>
2018-03-12 14:10:58 +0200

    Eliminated white spaces in the tables, added responsive design for time-zone panel

--- app/stock-panel-list/stock-panel-list.component.js
+++ app/stock-panel-list/stock-panel-list.component.js
@@ -7,13 +7,34 @@ angular
         controller: ['StockModel', 'stocksPerPage', function(Stock, stocksPerPage) {
             var $ctrl = this;
             this.$postLink = function toggleClass() {
-                if ($(window).width() < 800 && $(window).width() > 600) {
+                if ($(window).width() < 1550 && $(window).width() > 1200) {
+                    $('#toggle-panel').removeClass('time-panel col s3').addClass('time-panel col s4');
+                    $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s3 offset-s3');
+                    $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s1');
+                } else if ($(window).width() < 1200 && $(window).width() > 900) {
+                    $('#toggle-panel').removeClass('time-panel col s3').addClass('time-panel col s5');
+                    $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s3 offset-s2');
+                    $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s1');
+                } else if ($(window).width() < 900 && $(window).width() > 750) {
+                    $('#toggle-panel').removeClass('time-panel col s3').addClass('time-panel col s6');
+                    $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s3 offset-s1');
+                    $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s1');
+                } else if ($(window).width() < 750 && $(window).width() > 600) {
+                    $('#toggle-panel').removeClass('time-panel col s3').addClass('time-panel col s8 offset-s2');
                     $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s6 offset-s2');
-                    $('#toggle-btn').removeClass('input-field col s3').addClass('input-field col s4');
-                } else if ($(window).width() < 600) {
-                    $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s6 offset-s1');
-                    $('#toggle-btn').removeClass('input-field col s3').addClass('input-field col s5');
+                    $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s4');
+                } else if ($(window).width() < 600 && $(window).width() > 100) {
+                    $('#toggle-panel').removeClass('time-panel col s3').addClass('time-panel col s12');
+                    $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s8 offset-s1');
+                    $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s3');
                 }
+                // else if ($(window).width() < 800 && $(window).width() > 600) {
+                //     $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s6 offset-s2');
+                //     $('#toggle-btn').removeClass('input-field col s1').addClass('input-field col s4');
+                // } else if ($(window).width() < 600) {
+                //     $('#toggle-input').removeClass('input-field col s3 offset-s4').addClass('input-field col s6 offset-s1');
+                //     $('#toggle-btn').removeClass('input-field col s3').addClass('input-field col s5');
+                // }
             };
 
             this.stocks = [
