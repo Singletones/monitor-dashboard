2bb85b754192e4721240280f3594c9292015af61
Ilya <dadofasting@gmail.com>
2018-04-03 04:51:15 +0300

    Cells with the same price value, highlighted in the same color

--- src/components/order-book/order-book.component.js
+++ src/components/order-book/order-book.component.js
@@ -24,7 +24,53 @@ angular
                     }, (orderbooks) => {
                         $scope.$broadcast('orderBookLoaded');
                         $ctrl.stock.setOrderBooksData(orderbooks);
+                        $ctrl.colorPrices();
+
                     });
+                };
+
+                function shuffle(array) {
+                    for (let i = array.length - 1; i > 0; i--) {
+                        let j = Math.floor(Math.random() * (i + 1));
+                        let tmp = array[i];
+                        array[i] = array[j];
+                        array[j] = tmp;
+                    }
+                    return array;
+                };
+
+                var pricesColors = {},
+                    colorObj = {};
+
+                $ctrl.colorPrices = function(){
+                    let colors = shuffle(["#CCD34F","#03a9f4","#c41116","#0fa200","#0033CC","#CC0099","#FC9516",
+                        "#2D5B71","#712D60","#37898A","#BABC8E","#908EBC","#C194A3","#86711C","#3A1C86","#861C1C",
+                        "#64C3BE","#7C64C3","#29BC5C","#650A44"]);
+
+                    let trades = $ctrl.stock.getRecentOrderBook().getAsk().concat($ctrl.stock.getRecentOrderBook().getBid());
+
+                    let index = 0;
+                    trades.forEach(function(trade){
+
+                        if (pricesColors.hasOwnProperty(trade.Price)){
+                            // great!;
+                        }
+                        else {
+                            pricesColors[trade.Price] = colors[index];
+                            index++;
+                        }
+                    });
+                    return pricesColors;
+                };
+
+                $ctrl.askPriceColor = function(index) {
+                    colorObj = {'color': pricesColors[$ctrl.stock.getRecentOrderBook().getAsk()[index].Price], 'font-weight': 'bold'};
+                    return colorObj;
+                };
+
+                $ctrl.bidPriceColor = function(index) {
+                    colorObj = {'color': pricesColors[$ctrl.stock.getRecentOrderBook().getBid()[index].Price], 'font-weight': 'bold'};
+                    return colorObj;
                 };
 
                 $ctrl.$postLink = function () {
